1ff241afc6d00cf018f7ea8c0c15b91f32624b9d
David Lucas <davidlucaskl@gmail.com>
2024-01-18 12:37:41 -0300

    nav bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { NavBar } from './components/Navbar/Navbar.jsx';
-import { Search } from './pages/Search/Search.jsx';
-import Home from './pages/Home/Home.jsx';
-import { GlobalStyled } from "./GlobalStyled.jsx"
-import ErrorPage from './pages/ErrorPage/Error-Page.jsx';
+import { NavBar } from "./components/Navbar/Navbar.jsx";
+import { Search } from "./pages/Search/Search.jsx";
+import Home from "./pages/Home/Home.jsx";
+import { GlobalStyled } from "./GlobalStyled.jsx";
+import ErrorPage from "./pages/ErrorPage/Error-Page.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <NavBar/>,
-    errorElement: < ErrorPage />,
-    children: [{
-      path: "/",
-      element: <Home />,
-    }, {
-      path: "/search",
-      element: <Search/>
-    }],
+    element: <NavBar />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/search/:title",
+        element: <Search />,
+      },
+    ],
   },
 ]);
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <GlobalStyled />
+    <GlobalStyled />
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,3 +1,6 @@
+import { useParams } from "react-router-dom";
+
 export function Search() {
-    return <h1>Search</h1>
+  const { title } = useParams();
+  return <h1>{title}</h1>;
 }
