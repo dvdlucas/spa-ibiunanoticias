d5d796e7b6b40793f19d231c22bb61a482a37ff3
David Lucas <davidlucaskl@gmail.com>
2024-01-23 16:51:29 -0300

    função cadastrar usuário

---
+++ src/schemas/SearchSchema.js
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const searchSchema = z.object({
+    title: z.string()
+    .nonempty({message: "A pesquisa não pode ser vazia"})
+    .refine(value => !/^\s*$/.test(value), {message: "A pesquisa não pode ter apenas espaço"}),
+});
